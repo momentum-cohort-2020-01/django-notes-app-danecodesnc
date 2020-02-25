021f3f35084472de72e17e438a08d2b6d137004f
Dane <danecodesnc@gmail.com>
2020-02-25 10:19:24 -0500

    Work in class

--- core/views.py
+++ core/views.py
@@ -4,11 +4,16 @@ from django.http import HttpResponse
 
 import data
 
-
-def index(request):
+# We are learning function based views. Not using class based views yet.
+def  notes_list(request):
     notes = data.NOTES
     #The notes below is a dictionary.
-    return render(request, 'base.html', {'notes': notes})
+    return render(request, 'core/notes_list.html', {'notes': notes})
+
+def notes_detail(request, pk):
+    note = data.NOTES[str(pk)]
+    return render(request, 'core/notes_detail.html', {'note': note})
+
 
 
 # Create your views here.
