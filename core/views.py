aefb1c29398867b71a40c30c0707c92bac564c9b
Dane <danecodesnc@gmail.com>
2020-02-25 13:30:40 -0500

    Work in Progress

--- core/views.py
+++ core/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
-import data
+from .models import Note
 
-# We are learning function based views. Not using class based views yet.
+
 def  notes_list(request):
-    notes = data.NOTES
-    #The notes below is a dictionary.
+    notes = Note.objects.all()
     return render(request, 'core/notes_list.html', {'notes': notes})
 
 def notes_detail(request, pk):
-    note = data.NOTES[str(pk)]
-    return render(request, 'core/notes_detail.html', {'note': note})
-
-
-
-# Create your views here.
-
+    note = Note.objects.get(pk=pk)
+    return render(request, 'core/notes_detail.html', {'note': note, 'pk': pk})
