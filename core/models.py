d749b21f78556e899e3f710461d85081bac13818
Dane <danecodesnc@gmail.com>
2020-02-26 10:24:46 -0500

    Making progress

--- core/models.py
+++ core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Note(models.Model):
-    title = models.CharField(max_length=40)
-    body = models.TextField(max_length=None)
+    title = models.CharField(max_length=80)
+    body = models.TextField(max_length=300)
 
     def __str__(self):
         return f"Note item: {self.title} body: {self.body}"
